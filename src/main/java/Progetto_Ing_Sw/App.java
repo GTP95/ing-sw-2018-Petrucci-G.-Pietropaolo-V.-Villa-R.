1f011d324d5306dae4ba3db21a17d4b50dc49710
Giacomo Tommaso Petrucci <giacomo.tom.petrucci@tiscali.it>
2018-03-26 17:37:03 +0200

    Initial commit, basic Maven configuration

---
+++ src/main/java/Progetto_Ing_Sw/App.java
@@ -0,0 +1,13 @@
+package Progetto_Ing_Sw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
