a3dbefe929d2032231e1a9785f0841f643c38451
Roberto Villa <roberto12.villa@mail.polimi.it>
2018-04-22 11:15:10 +0200

    Updating GameBoardCard/Deck and creating GameBoardCardDeckTest class

--- src/main/java/Progetto_Ing_Sw/com/server/GameBoardCardDeck.java
+++ src/main/java/Progetto_Ing_Sw/com/server/GameBoardCardDeck.java
@@ -1,4 +1,33 @@
 package Progetto_Ing_Sw.com.server;
 
-public class GameBoardCardDeck {
+import java.util.ArrayList;
+import java.util.SplittableRandom;
+
+public class GameBoardCardDeck extends Deck{
+    private ArrayList<GameBoardCard> cards;
+
+    public GameBoardCardDeck(ArrayList<GameBoardCard> cards) { this.cards = cards;}
+
+    @Override
+    public GameBoardCard draw(){
+        GameBoardCard card=null;
+        SplittableRandom splittableRandom = new SplittableRandom();
+        int index=0;
+
+        while(card==null){  //isEmpty?
+            index=splittableRandom.nextInt(0,cards.size());
+            card=cards.get(index);
+        }
+        cards.remove(index);
+        return card;
+    }
+
+    @Override
+    public ArrayList<Card> draw(int cards){
+        ArrayList<Card> cardsArrayList=new ArrayList<>();
+        for(int counter=0;counter<cards;counter++){
+            cardsArrayList.add(draw());
+        }
+        return cardsArrayList;
+    }
 }
