020ba3e78735db438fab19d2252c1040c68ac167
Roberto Villa <roberto12.villa@mail.polimi.it>
2018-05-22 17:27:21 +0200

    Iniziato con l'implementazione dell'MVC lato client

--- src/main/java/Progetto_Ing_Sw/com/client/LaunchClient.java
+++ src/main/java/Progetto_Ing_Sw/com/client/LaunchClient.java
@@ -1,28 +1,26 @@
 package Progetto_Ing_Sw.com.client;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.util.Scanner;
 
 public class LaunchClient {
+    public static void main(String args[]) throws IOException{
 
-    public static void main(String args[]) {
-        try {
-            Socket clientSocket = new Socket("localhost", 1024);
-            PrintWriter out=new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            Scanner scanner=new Scanner(System.in);
-            out.println(scanner.nextLine());
-            System.out.println(in.readLine());
+    //parte che importa host/port da file
+    //righe successive da eleiminare dopo aver fatto la parte sopra
+    String host="localhost";
+    int port=1024;
 
-            try{Thread.sleep(60000);}   //solo per test multithread
-            catch(InterruptedException e){e.printStackTrace();}
-        }
-        catch(IOException e){
-            e.getStackTrace();
-        }
+    //creazione del client
+    Client client=new Client(host,port);
+    ClientController clientController= new ClientController(client);
+
+    try {
+        client.startSocket();
+        clientController.startGame();
+
+        try{Thread.sleep(60000);}   //solo per test multithread
+        catch(InterruptedException e){e.printStackTrace();}
+
+    }catch (IOException e) {e.printStackTrace();}
+
     }
 }
