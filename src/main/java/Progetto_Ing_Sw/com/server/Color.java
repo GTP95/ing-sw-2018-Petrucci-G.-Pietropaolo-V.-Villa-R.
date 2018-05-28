ebaf0fe11f60689d014a169c0362bf70da222030
Roberto Villa <roberto12.villa@mail.polimi.it>
2018-05-28 16:46:09 +0200

    Modifice per l'import della matrice e le sue sfumature.

--- src/main/java/Progetto_Ing_Sw/com/server/Color.java
+++ src/main/java/Progetto_Ing_Sw/com/server/Color.java
@@ -1,9 +1,9 @@
 package Progetto_Ing_Sw.com.server;
 
 public final class Color {
-    public static final int RED=0;
-    public static final int BLUE=1;
-    public static final int PURPLE=2;
-    public static final int YELLOW=3;
-    public static final int GREEN=4;
+    public static final int RED=1;
+    public static final int BLUE=2;
+    public static final int PURPLE=3;
+    public static final int YELLOW=4;
+    public static final int GREEN=5;
 }
