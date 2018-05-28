d9a9da87911153eb5eef002b990189b6f00a11a8
Giacomo Tommaso Petrucci <giacomo.tom.petrucci@tiscali.it>
2018-05-28 15:53:21 +0200

    Gestione utente e porta all'interno di classe Model del client

--- src/main/java/Progetto_Ing_Sw/com/client/Model.java
+++ src/main/java/Progetto_Ing_Sw/com/client/Model.java
@@ -1,13 +1,27 @@
 package Progetto_Ing_Sw.com.client;
 
+import Progetto_Ing_Sw.com.tools.JSONCreator;
+
+import java.io.FileNotFoundException;
+
 public  class Model {
     private  String username;
     private static Model ourInstance=new Model();
+    private String hostname;
+    private int port;
 
 
     private Model(){
         username=null;
         ourInstance=this;
+        try {
+            hostname = JSONCreator.parseStringFieldFromFile("src/main/java/Progetto_Ing_Sw/com/client/Settings/ClientSettings.json", "host");
+            port=JSONCreator.parseIntFieldFromFile("src/main/java/Progetto_Ing_Sw/com/client/Settings/ClientSettings.json","port");
+        }
+        catch (FileNotFoundException e){
+            hostname="localhost";
+            port=1024;
+        }
     }
 
     public static Model getInstance(){
@@ -22,5 +36,21 @@ public  class Model {
 
     public synchronized String getUsername() {
         return username;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
     }
 }
