567b79f1d22dabb1c31f1a83dff339abbbd03f80
Giacomo <giacomo.tom.petrucci@tiscali.it>
2018-06-15 17:23:12 +0200

    Aggiunta classe ClientColor per poter usare le costanti anche lato client

---
+++ src/main/java/Progetto_Ing_Sw/com/client/ClientColor.java
@@ -0,0 +1,11 @@
+package Progetto_Ing_Sw.com.client;
+
+public final class ClientColor {
+    public static final int BLANK=0;
+    public static final int RED=1;
+    public static final int BLUE=2;
+    public static final int PURPLE=3;
+    public static final int YELLOW=4;
+    public static final int GREEN=5;
+    public static final int SHADE=23;
+}
