43e1b2c1c188457ef16ebe5312270e40d332d747
Roberto Villa <roberto12.villa@mail.polimi.it>
2018-06-23 12:46:50 +0200

    Merge branch 'master' of C:\Users\RobyV\Documents\PoliMi\Anno 2017-2018\Secondo Semestre\Prova Finale\ing-sw-2018-Petrucci-G.-Pietropaolo-V.-Villa-R with conflicts.

--- src/main/java/Progetto_Ing_Sw/com/server/Model/PublicObjectiveCards/ColorDiagonals.java
+++ src/main/java/Progetto_Ing_Sw/com/server/Model/PublicObjectiveCards/ColorDiagonals.java
@@ -1,4 +1,32 @@
 package Progetto_Ing_Sw.com.server.Model.PublicObjectiveCards;
 
+import Progetto_Ing_Sw.com.server.Model.WindowBoard;
+import Progetto_Ing_Sw.com.tools.JSONCreator;
+
+import java.io.FileNotFoundException;
+
 public class ColorDiagonals {
+
+    //-------import del costo di primo uso
+    private int localVictoryPoints;
+    {try {
+        localVictoryPoints = JSONCreator.parseIntFieldFromFile("Resources/Cards/PublicObjectiveCards/ColorVariety.json","victoryPoints" );
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();}
+    }
+    //-----------------
+
+    private int victoryPoints;
+    public ColorDiagonals() {
+        this.victoryPoints = localVictoryPoints;
+    }
+
+    public int getVictoryPoints() {return victoryPoints;}
+
+    public int calculatePoints(WindowBoard localBoard) {
+
+        int points=0;
+
+        return points;
+    }
 }
