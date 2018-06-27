2f811e4bdf72f8146f98a043c6d4a16ad63a869b
Giacomo <giacomo.tom.petrucci@tiscali.it>
2018-06-27 12:51:11 +0200

    Commit per poter fare push senza sovrascrivere cambiamenti (chiamata updateTable)

--- src/main/java/Progetto_Ing_Sw/com/client/LocalModel.java
+++ src/main/java/Progetto_Ing_Sw/com/client/LocalModel.java
@@ -279,6 +279,7 @@ public  class LocalModel {
 
     public void setNumOfWindowBoards(int numOfWindowBoards) {
         this.numOfWindowBoards = numOfWindowBoards;
+        System.err.println("numofwindowboards settaato a "+numOfWindowBoards);
     }
 
     public void setChoosenGameBoardCard(ClientGameBoardCard choosenGameBoardCard) {
@@ -332,13 +333,13 @@ public  class LocalModel {
     public void addUpdatedWindowBoard(ClientWindowBoard windowBoard){
         if(updatedWindowBoards==null) updatedWindowBoards=new ArrayList<>();
         updatedWindowBoards.add(windowBoard);
-        if (updatedWindowBoards.size()==numOfWindowBoards){
+        if (updatedWindowBoards.size()==numOfWindowBoards){ //qui c'è il problema perchè viene eseguito prima di riceverle tutte
             System.err.println("Ricevute tutte le gameboardcard");
             for(int index=0;index<clientPlayerArrayList.size();index++) clientPlayerArrayList.get(index).updateWindowBoard(updatedWindowBoards.get(index));
             if(firstWindowBoardsReceived){
                 chooseAWindowobserver.StartTable();
                 while (tableGUIobserver==null); //aspetta che tableGui si registri come observer
-                tableGUIobserver.updateTable();
+               // tableGUIobserver.updateTable();
                 firstWindowBoardsReceived=false;
             }
 
