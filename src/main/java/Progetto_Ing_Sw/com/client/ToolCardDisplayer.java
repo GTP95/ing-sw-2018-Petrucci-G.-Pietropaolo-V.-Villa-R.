0284a76579f82198989f376300b923faf6eba4ff
uugui <vincenzopietropaolo@gmail.com>
2018-06-28 13:13:06 +0200

    ToolCardDisplayer: - Aggiunta Grafica per 4 nuove carte: Glazing Hammer, Flux Brush, Flux Remover, Grinding Stone

--- src/main/java/Progetto_Ing_Sw/com/client/ToolCardDisplayer.java
+++ src/main/java/Progetto_Ing_Sw/com/client/ToolCardDisplayer.java
@@ -1,5 +1,6 @@
 package Progetto_Ing_Sw.com.client;
 
+import Progetto_Ing_Sw.com.server.Model.ToolCards.GlazingHammer;
 import javafx.animation.TranslateTransition;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -19,6 +20,7 @@ import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ToolCardDisplayer extends Stage {
     Scene ToolCardDisplay;
@@ -27,7 +29,52 @@ public class ToolCardDisplayer extends Stage {
     int valoredado =0;
     String GrozingCommand;
     Label DieChoosen;
+    ArrayList<ToggleButton> DiceButtons;
+    Random randomValue;
+    Random randomColor;
     static final Image windowIcon = new Image("Progetto_Ing_Sw/com/client/GUI/GameIcon.png");
+
+    public VBox getDraftPool(){
+        VBox GrozingPool = new VBox(5);
+        GrozingPool.setTranslateX(50);
+        GrozingPool.setPadding(new Insets(4, 4, 4, 4));
+        GrozingPool.setAlignment(Pos.CENTER);
+        GrozingPool.setMaxWidth(100);
+
+        ToggleGroup  Dicegroup = new ToggleGroup();
+        DiceButtons = new ArrayList<>();
+        for (ClientDice dice : LocalModel.getInstance().getDrawnDice()) {
+            ToggleButton Die = new ToggleButton();
+            Die.setPrefSize(75, 75);
+            Die.setMaxSize(75, 75);
+            Die.setId("Die");
+            Die.setText("DiePresente");
+            Die.setToggleGroup(Dicegroup);
+            GrozingPool.getChildren().addAll(Die);
+            DiceButtons.add(Die);
+        }
+        for (int i = 0; i < DiceButtons.size(); i++) {
+            int LocalValue = LocalModel.getInstance().getDrawnDice().get(i).getValue();
+            int LocalColor = LocalModel.getInstance().getDrawnDice().get(i).getColor();
+            if (LocalValue == 0) {
+                DiceButtons.get(i).setVisible(false);
+            } else {
+                DiceButtons.get(i).setText("");
+                DiceButtons.get(i).setId(Integer.toString(LocalValue) + new ClientColor().IntToColor(LocalColor));
+                DiceButtons.get(i).setOnAction(event -> {
+                            DieToInsert = new ClientDice(LocalValue, LocalColor);
+                            valoredado = DieToInsert.getValue();
+                            DieChoosen.setId(Integer.toString(DieToInsert.getValue()) + new ClientColor().IntToColor(DieToInsert.getColor()));
+                            System.out.println("Die choosen: " + Integer.toString(LocalValue) + "," + Integer.toString(LocalColor));
+                        }
+                );
+            }
+            Tooltip t = new Tooltip(("Color: " + new ClientColor().IntToColor(LocalColor) + "\n" + "Value: " + Integer.toString(LocalValue)));
+            Tooltip.install(DiceButtons.get(i), t);
+        }
+
+        return GrozingPool;
+    }
 
     ToolCardDisplayer(String Title, int Number, String Description, String Info, String CardColor, boolean FirstUsage) {
         this.setTitle(Title);
@@ -176,6 +223,12 @@ public class ToolCardDisplayer extends Stage {
 
         StackPane ToolCardDisplayerSecond = new StackPane();
 
+        DieChoosen = new Label();
+        DieChoosen.setMaxSize(100, 100);
+        DieChoosen.setStyle("-fx-background-size: 100 100;");
+        DieChoosen.setId("trasnparent");
+        DieChoosen.setTranslateX(430);
+
 
         //----------------------------------------------------------------------------------INIZIO GROZING PLIERS----------------------------------------------------------------------------------//
         Text TitleInfo = new Text("Choose a Die to Increase or Decrease");
@@ -185,29 +238,9 @@ public class ToolCardDisplayer extends Stage {
 
 
 
-        VBox GrozingPool = new VBox(5);
-        GrozingPool.setTranslateX(50);
-        GrozingPool.setPadding(new Insets(4, 4, 4, 4));
-        GrozingPool.setAlignment(Pos.CENTER);
-        GrozingPool.setMaxWidth(100);
+        VBox GrozingPool = getDraftPool();
 
 
-        ArrayList<ToggleButton> DiceButtons = new ArrayList<>();
-        for (ClientDice dice : LocalModel.getInstance().getDrawnDice()) {
-            ToggleButton Die = new ToggleButton();
-            Die.setPrefSize(75, 75);
-            Die.setMaxSize(75, 75);
-            Die.setId("Die");
-            Die.setText("DiePresente");
-            GrozingPool.getChildren().addAll(Die);
-            DiceButtons.add(Die);
-        }
-
-        DieChoosen = new Label();
-        DieChoosen.setMaxSize(100, 100);
-        DieChoosen.setStyle("-fx-background-size: 100 100;");
-        DieChoosen.setId("transparent");
-        DieChoosen.setTranslateX(430);
 
         Button AcceptGrozing = new Button();
         AcceptGrozing.setVisible(false);
@@ -241,6 +274,59 @@ public class ToolCardDisplayer extends Stage {
             AcceptGrozing.setVisible(true);
         });
 
+
+
+        StackPane GrozingPliers = new StackPane();
+        GrozingPliers.setPrefSize(1280, 720);
+
+        //----------------------------------------------------------------------------------FINE GROZING PLIERS----------------------------------------------------------------------------------//
+
+        //----------------------------------------------------------------------------------INIZIO ENGLOMISE BRUSH----------------------------------------------------------------------------------//
+
+
+
+        //----------------------------------------------------------------------------------FINE ENGLOMISE BRUSH----------------------------------------------------------------------------------//
+
+        //----------------------------------------------------------------------------------INIZIO GRINDING STONE----------------------------------------------------------------------------------//
+        Text GrindingStoneInfo = new Text("Choose a Die to Flip");
+        GrindingStoneInfo.setStyle("-fx-fill: white;");
+        GrindingStoneInfo.setTranslateX(380);
+        GrindingStoneInfo.setTranslateY(-325);
+
+        VBox GrindingStonePool = getDraftPool();
+
+        Button AcceptGridingStone = new Button();
+        AcceptGridingStone.setId("NextBTN");
+        AcceptGridingStone.setMaxSize(150, 150);
+        AcceptGridingStone.setVisible(false);
+        AcceptGridingStone.setTranslateX(600);
+
+        Button FlipButton = new Button("Flip Die");
+        FlipButton.setId("transparent");
+        FlipButton.setStyle("-fx-font: 25 'Castellar';");
+        FlipButton.setTranslateX(430);
+        FlipButton.setTranslateY(100);
+        FlipButton.setVisible(true);
+        FlipButton.setOnAction(event -> {
+            switch (valoredado){
+                case(1):DieChoosen.setId(Integer.toString(6)+ new ClientColor().IntToColor(DieToInsert.getColor()));
+                break;
+                case(2):DieChoosen.setId(Integer.toString(5)+ new ClientColor().IntToColor(DieToInsert.getColor()));
+                break;
+                case (3):DieChoosen.setId(Integer.toString(4)+ new ClientColor().IntToColor(DieToInsert.getColor()));
+                break;
+                case (4):DieChoosen.setId(Integer.toString(3)+ new ClientColor().IntToColor(DieToInsert.getColor()));
+                break;
+                case(5):DieChoosen.setId(Integer.toString(2)+ new ClientColor().IntToColor(DieToInsert.getColor()));
+                break;
+                case(6):DieChoosen.setId(Integer.toString(1)+ new ClientColor().IntToColor(DieToInsert.getColor()));
+            }
+            FlipButton.setDisable(true);
+            AcceptGridingStone.setVisible(true);
+        });
+
+
+
         for (int i = 0; i < DiceButtons.size(); i++) {
             int LocalValue = LocalModel.getInstance().getDrawnDice().get(i).getValue();
             int LocalColor = LocalModel.getInstance().getDrawnDice().get(i).getColor();
@@ -252,8 +338,7 @@ public class ToolCardDisplayer extends Stage {
                 DiceButtons.get(i).setOnAction(event -> {
                             DieToInsert = new ClientDice(LocalValue, LocalColor);
                             valoredado = DieToInsert.getValue();
-                            IncreaseValue.setVisible(true);
-                            DecreaseValue.setVisible(true);
+                            FlipButton.setVisible(true);
                             DieChoosen.setId(Integer.toString(DieToInsert.getValue()) + new ClientColor().IntToColor(DieToInsert.getColor()));
                             System.out.println("Die choosen: " + Integer.toString(LocalValue) + "," + Integer.toString(LocalColor));
                         }
@@ -263,20 +348,216 @@ public class ToolCardDisplayer extends Stage {
             Tooltip.install(DiceButtons.get(i), t);
         }
 
-        StackPane GrozingPliers = new StackPane();
-        GrozingPliers.setPrefSize(1280, 720);
-        GrozingPliers.getChildren().addAll(TitleInfo, GrozingPool, IncreaseValue, DecreaseValue, DieChoosen, AcceptGrozing);
-        //----------------------------------------------------------------------------------FINE GROZING PLIERS----------------------------------------------------------------------------------//
-
-        //----------------------------------------------------------------------------------INIZIO ENGLOMISE BRUSH----------------------------------------------------------------------------------//
+        StackPane GridingStone = new StackPane();
+        GridingStone.setPrefSize(1280,720);
 
 
 
-        //----------------------------------------------------------------------------------FINE ENGLOMISE BRUSH----------------------------------------------------------------------------------//
+        //----------------------------------------------------------------------------------FINE GRINDING STONE----------------------------------------------------------------------------------//
+
+        //----------------------------------------------------------------------------------INIZIO FLUX REMOVER----------------------------------------------------------------------------------//
+
+        Text FluxRemoverInfo = new Text("Choose to return to the Dice Bag");
+        FluxRemoverInfo.setStyle("-fx-fill: white;");
+        FluxRemoverInfo.setTranslateX(380);
+        FluxRemoverInfo.setTranslateY(-325);
+
+        VBox FluxRemoverPool = getDraftPool();
+
+        Label DiceBag = new Label();
+        DiceBag.setPrefSize(400,400);
+        DiceBag.setId("DiceBag");
+        DiceBag.setTranslateX(430);
+        DiceBag.setTranslateY(300);
 
 
 
-        ToolCardDisplayerSecond.getChildren().addAll(GrozingPliers,ToolCardD1);
+
+        Button AcceptBag = new Button();
+        AcceptBag.setId("NextBTN");
+        AcceptBag.setMaxSize(150, 150);
+        AcceptBag.setTranslateX(150);
+        AcceptBag.setOnAction(event -> {
+            TranslateTransition DiceBagTransition = new TranslateTransition(Duration.millis(500),DieChoosen);
+            DiceBagTransition.setFromY(720);
+            DieChoosen.setVisible(true);
+            DiceBagTransition.setToY(0);
+            DiceBagTransition.setAutoReverse(false);
+            DiceBagTransition.play();
+        });
+
+        Button AcceptFluxRemover = new Button();
+        AcceptFluxRemover.setId("NextBTN");
+        AcceptFluxRemover.setMaxSize(150, 150);
+        AcceptFluxRemover.setTranslateX(600);
+
+        Button IncreaseFluxRemover = new Button();
+        IncreaseFluxRemover.setVisible(false);
+        IncreaseFluxRemover.setMaxSize(200, 200);
+        IncreaseFluxRemover.setId("IncreaseBTN");
+        IncreaseFluxRemover.setTranslateY(-100);
+        IncreaseFluxRemover.setTranslateX(430);
+        IncreaseFluxRemover.setOnAction(event -> {
+            DieChoosen.setId(Integer.toString(valoredado++) + new ClientColor().IntToColor(DieToInsert.getColor()));
+            valoredado= valoredado++;
+            if (valoredado==6){
+                IncreaseFluxRemover.setVisible(false);
+            }
+            else {IncreaseFluxRemover.setVisible(true);}
+            AcceptFluxRemover.setVisible(true);
+        });
+
+        Button DecreaseFluxRemover = new Button();
+        DecreaseFluxRemover.setVisible(false);
+        DecreaseFluxRemover.setMaxSize(200, 200);
+        DecreaseFluxRemover.setId("IncreaseBTN");
+        DecreaseFluxRemover.setTranslateY(-100);
+        DecreaseFluxRemover.setTranslateX(430);
+        DecreaseFluxRemover.setOnAction(event -> {
+            DieChoosen.setId(Integer.toString(valoredado--) + new ClientColor().IntToColor(DieToInsert.getColor()));
+            valoredado= valoredado--;
+            if (valoredado==1){
+                DecreaseFluxRemover.setVisible(false);
+            }
+            else {DecreaseFluxRemover.setVisible(true);}
+            AcceptFluxRemover.setVisible(true);
+        });
+
+        StackPane FluxRemover = new StackPane();
+        FluxRemover.setPrefSize(1280,720);
+
+
+        //----------------------------------------------------------------------------------FINE FLUX REMOVER----------------------------------------------------------------------------------//
+
+        //----------------------------------------------------------------------------------INIZIO FLUX BRUSH----------------------------------------------------------------------------------//
+
+        Text FluxBrushInfo = new Text("Choose a Die to Re-Roll");
+        FluxBrushInfo.setStyle("-fx-fill: white;");
+        FluxBrushInfo.setTranslateX(380);
+        FluxBrushInfo.setTranslateY(-325);
+
+        VBox FluxBrushPool = getDraftPool();
+
+        Button FluxBrushAccept = new Button();
+        FluxBrushAccept.setTranslateX(600);
+        FluxBrushAccept.setMaxSize(150,150);
+        FluxBrushAccept.setVisible(false);
+        FluxBrushAccept.setId("NextBTN");
+
+        Button ReRollButton = new Button("Re-Roll");
+        ReRollButton.setStyle("-fx-font: 25 'Castellar';");
+        ReRollButton.setTranslateX(430);
+        ReRollButton.setTranslateY(100);
+        ReRollButton.setVisible(true);
+        ReRollButton.setOnAction(event -> {
+            //TODO random con roberto
+            FluxBrushAccept.setVisible(true);
+            ReRollButton.setDisable(true);
+        });
+
+
+        StackPane FluxBrush = new StackPane();
+        FluxBrush.setPrefSize(1280,720);
+
+
+        //----------------------------------------------------------------------------------FINE FLUX BRUSH----------------------------------------------------------------------------------//
+
+        //----------------------------------------------------------------------------------INIZIO GLAZING HAMMER----------------------------------------------------------------------------------//
+
+        Text GlazingHammerInfo = new Text("Press Re-Roll to re-roll all Dice");
+        GlazingHammerInfo.setStyle("-fx-fill: white;");
+        GlazingHammerInfo.setTranslateX(380);
+        GlazingHammerInfo.setTranslateY(-325);
+
+        Button GlazingHammerAccept = new Button();
+        GlazingHammerAccept.setId("NextBTN");
+        GlazingHammerAccept.setMaxSize(150, 150);
+        GlazingHammerAccept.setTranslateX(600);
+
+
+        FlowPane GlazingHammerPool = new FlowPane();
+        GlazingHammerPool.setTranslateX(400);
+        GlazingHammerPool.setVgap(5);
+        GlazingHammerPool.setHgap(5);
+        GlazingHammerPool.setPadding(new Insets(4, 4, 4, 4));
+        GlazingHammerPool.setAlignment(Pos.CENTER);
+        GlazingHammerPool.setMaxSize(400,400);
+
+        ToggleGroup  Dicegroup1 = new ToggleGroup();
+        DiceButtons = new ArrayList<>();
+        for (ClientDice dice : LocalModel.getInstance().getDrawnDice()) {
+            ToggleButton Die = new ToggleButton();
+            Die.setPrefSize(75, 75);
+            Die.setMaxSize(75, 75);
+            Die.setId("Die");
+            Die.setText("DiePresente");
+            Die.setToggleGroup(Dicegroup1);
+            GlazingHammerPool.getChildren().addAll(Die);
+            DiceButtons.add(Die);
+        }
+        for (int i = 0; i < DiceButtons.size(); i++) {
+            int LocalValue = LocalModel.getInstance().getDrawnDice().get(i).getValue();
+            int LocalColor = LocalModel.getInstance().getDrawnDice().get(i).getColor();
+            if (LocalValue == 0) {
+                DiceButtons.get(i).setVisible(false);
+            } else {
+                DiceButtons.get(i).setText("");
+                DiceButtons.get(i).setId(Integer.toString(LocalValue) + new ClientColor().IntToColor(LocalColor));
+                DiceButtons.get(i).setOnAction(event -> {
+                            DieToInsert = new ClientDice(LocalValue, LocalColor);
+                            valoredado = DieToInsert.getValue();
+                            DieChoosen.setId(Integer.toString(DieToInsert.getValue()) + new ClientColor().IntToColor(DieToInsert.getColor()));
+                            System.out.println("Die choosen: " + Integer.toString(LocalValue) + "," + Integer.toString(LocalColor));
+                        }
+                );
+            }
+            Tooltip t = new Tooltip(("Color: " + new ClientColor().IntToColor(LocalColor) + "\n" + "Value: " + Integer.toString(LocalValue)));
+            Tooltip.install(DiceButtons.get(i), t);
+        }
+
+        Button GlazingHammerReRollButton = new Button("Re-Roll ");
+        GlazingHammerReRollButton.setStyle("-fx-font: 25 'Castellar';");
+        GlazingHammerReRollButton.setTranslateX(400);
+        GlazingHammerReRollButton.setTranslateY(150);
+        GlazingHammerReRollButton.setVisible(true);
+        GlazingHammerReRollButton.setOnAction(event -> {
+            //TODO random con roberto
+            GlazingHammerAccept.setVisible(true);
+            GlazingHammerReRollButton.setDisable(true);
+        });
+
+        StackPane GlazingHammer = new StackPane();
+        GlazingHammer.setPrefSize(1280,720);
+
+        //----------------------------------------------------------------------------------FINE GLAZING HAMMER----------------------------------------------------------------------------------//
+
+        //ToolCardDisplayerSecond.getChildren().addAll(ToolCardD1);
+        switch (Title){
+            case ("Grozing Pliers"):
+                GrozingPliers.getChildren().addAll(TitleInfo, GrozingPool, IncreaseValue, DecreaseValue, DieChoosen, AcceptGrozing);
+                ToolCardDisplayerSecond.getChildren().addAll(GrozingPliers,ToolCardD1);
+                break;
+            case ("Grinding Stone"):
+                GridingStone.getChildren().addAll(GrindingStoneInfo,GrindingStonePool,AcceptGridingStone,FlipButton,DieChoosen);
+                ToolCardDisplayerSecond.getChildren().addAll(GridingStone,ToolCardD1);
+                break;
+            case ("Flux Remover"):
+                FluxRemover.getChildren().addAll(FluxRemoverInfo,FluxRemoverPool,AcceptFluxRemover,IncreaseFluxRemover,DecreaseFluxRemover,DieChoosen,DiceBag,AcceptBag);
+                ToolCardDisplayerSecond.getChildren().addAll(FluxRemover,ToolCardD1);
+                break;
+            case ("Flux Brusher"):
+                FluxBrush.getChildren().addAll(FluxBrushAccept,FluxBrushInfo,FluxBrushPool,ReRollButton,DieChoosen);
+                ToolCardDisplayerSecond.getChildren().addAll(FluxBrush,ToolCardD1);
+                break;
+            case ("Glazing Hammer"):
+                GlazingHammer.getChildren().addAll(GlazingHammerInfo,GlazingHammerAccept,GlazingHammerPool,GlazingHammerReRollButton);
+                ToolCardDisplayerSecond.getChildren().addAll(GlazingHammer,ToolCardD1);
+                break;
+        }
+
+
+
+
         ToolCardDisplayerSecond.setId("ToolCardDisplayer");
 
 
