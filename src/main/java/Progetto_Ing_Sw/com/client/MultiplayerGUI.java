b97a565f1c3ecbb1a8b0999e99e42eaddc634b32
uugui <vincenzopietropaolo@gmail.com>
2018-07-01 19:18:18 +0200

    MultiplayerGUI -  Nuova Lobby

--- src/main/java/Progetto_Ing_Sw/com/client/MultiplayerGUI.java
+++ src/main/java/Progetto_Ing_Sw/com/client/MultiplayerGUI.java
@@ -1,6 +1,7 @@
 package Progetto_Ing_Sw.com.client;
 
 import javafx.animation.PauseTransition;
+import javafx.animation.RotateTransition;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.geometry.Pos;
@@ -70,25 +71,25 @@ public class MultiplayerGUI extends Stage {
         //INIZIO Lobby Scene
 
         //Finestre
-        ImageView Player1Window = new ImageView("Progetto_Ing_Sw/com/client/GUI/LobbyPlayer1.png");Player1Window.setTranslateY(-30);
-        ImageView Player2Window = new ImageView("Progetto_Ing_Sw/com/client/GUI/LobbyPlayer2.png");Player2Window.setTranslateY(-30);
-        ImageView Player3Window = new ImageView("Progetto_Ing_Sw/com/client/GUI/LobbyPlayer3.png");Player3Window.setTranslateY(-30);
-        ImageView Player4Window = new ImageView("Progetto_Ing_Sw/com/client/GUI/LobbyPlayer4.png");Player4Window.setTranslateY(-30);
+        ImageView Loading1Icon = new ImageView("Progetto_Ing_Sw/com/client/GUI/Loading1.png");Loading1Icon.setTranslateY(0);
+        ImageView Loading2 = new ImageView("Progetto_Ing_Sw/com/client/GUI/Loading2.png");Loading2.setTranslateY(0);
+        ImageView Loading3 = new ImageView("Progetto_Ing_Sw/com/client/GUI/Loading3.png");Loading3.setTranslateY(0);
+        ImageView Loading4 = new ImageView("Progetto_Ing_Sw/com/client/GUI/Loading4.png");Loading4.setTranslateY(0);
 
 
         //Bottoni per i singoli giocatori da premere per dare il Ready
-        Player1BTN = new Label("Waiting..."); Player1BTN.setId("WaitingBTN");Player1BTN.setTranslateY(155);Player1BTN.setPrefSize(200,180);Player1BTN.setDisable(true);
-        Player2BTN = new Label("Waiting..."); Player2BTN.setId("WaitingBTN");Player2BTN.setTranslateY(155);Player2BTN.setPrefSize(200,180);Player2BTN.setDisable(true);
-        Player3BTN = new Label("Waiting..."); Player3BTN.setId("WaitingBTN");Player3BTN.setTranslateY(155);Player3BTN.setPrefSize(200,180);Player3BTN.setDisable(true);
-        Player4BTN = new Label("Waiting..."); Player4BTN.setId("WaitingBTN");Player4BTN.setTranslateY(155);Player4BTN.setPrefSize(200,180);Player4BTN.setDisable(true);
+        Player1BTN = new Label(); Player1BTN.setId("WaitingPlayer1");Player1BTN.setTranslateY(155);Player1BTN.setPrefSize(150,150);Player1BTN.setDisable(true);
+        Player2BTN = new Label(); Player2BTN.setId("WaitingPlayer2");Player2BTN.setTranslateY(155);Player2BTN.setPrefSize(150,150);Player2BTN.setDisable(true);
+        Player3BTN = new Label(); Player3BTN.setId("WaitingPlayer3");Player3BTN.setTranslateY(155);Player3BTN.setPrefSize(150,150);Player3BTN.setDisable(true);
+        Player4BTN = new Label(); Player4BTN.setId("WaitingPlayer4");Player4BTN.setTranslateY(155);Player4BTN.setPrefSize(150,150);Player4BTN.setDisable(true);
 
 
 
         //Labels con i nomi dei giocatori
-        Player1Label = new Label("Player 1"); Player1Label.setId("PlayerLobbyLabel");Player1Label.setPrefWidth(240);Player1Label.setTranslateY(260);
-        Player2Label = new Label("Player 2"); Player2Label.setId("PlayerLobbyLabel");Player2Label.setPrefWidth(240);Player2Label.setTranslateY(260);
-        Player3Label = new Label("Player 3"); Player3Label.setId("PlayerLobbyLabel");Player3Label.setPrefWidth(240);Player3Label.setTranslateY(260);
-        Player4Label = new Label("Player 4"); Player4Label.setId("PlayerLobbyLabel");Player4Label.setPrefWidth(240);Player4Label.setTranslateY(260);
+        Player1Label = new Label("Player 1"); Player1Label.setId("TextField");Player1Label.setPrefWidth(240);Player1Label.setTranslateY(200);
+        Player2Label = new Label("Player 2"); Player2Label.setId("TextField");Player2Label.setPrefWidth(240);Player2Label.setTranslateY(200);
+        Player3Label = new Label("Player 3"); Player3Label.setId("TextField");Player3Label.setPrefWidth(240);Player3Label.setTranslateY(200);
+        Player4Label = new Label("Player 4"); Player4Label.setId("TextField");Player4Label.setPrefWidth(240);Player4Label.setTranslateY(200);
 
 
 
@@ -97,50 +98,56 @@ public class MultiplayerGUI extends Stage {
 
 
         //StackPane Player1
-        StackPane Player1V = new StackPane();
-        Player1V.getChildren().addAll(Player1BTN,Player1Label,Player1Window);
+        HBox Player1V = new HBox(30);
+        Player1V.getChildren().addAll(Player1Label,Player1BTN);
+        Player1V.setTranslateX(50);
 
 
         //StackPane Player2
-        StackPane Player2V = new StackPane();
-        Player2V.setAlignment(Pos.CENTER);
-        Player2V.getChildren().addAll(Player2BTN,Player2Label,Player2Window);
+        HBox Player2V = new HBox(30);
+        Player2V.getChildren().addAll(Player2Label,Player2BTN);
+        Player2V.setTranslateX(50);
+        Player2V.setTranslateY(250);
 
 
         //StackPane Player3
-        StackPane Player3V = new StackPane();
-        Player3V.setAlignment(Pos.CENTER);
-        Player3V.getChildren().addAll(Player3BTN,Player3Label,Player3Window);
+        HBox Player3V = new HBox(30);
+        Player3V.getChildren().addAll(Player3BTN,Player3Label);
+        Player3V.setTranslateX(800);
+
 
         //StackPane Player4
-        StackPane Player4V = new StackPane();
-        Player4V.setAlignment(Pos.CENTER);
-        Player4V.getChildren().addAll(Player4BTN,Player4Label,Player4Window);
+        HBox Player4V = new HBox(30);
+        Player4V.getChildren().addAll(Player4BTN,Player4Label);
+        Player4V.setTranslateX(800);
+        Player4V.setTranslateY(250);
 
         //HBox per tutti i giocatori
-        HBox Players = new HBox(80);
+        StackPane Players = new StackPane();
         Players.setAlignment(Pos.CENTER);
         Players.getChildren().addAll(Player1V,Player2V,Player3V,Player4V);
 
-        //Timer
-
+        //Timer Label
         TimerLabel = new Label("");
         TimerLabel.setId("Timer");
-        TimerLabel.setPrefWidth(100);
-        TimerLabel.setTranslateY(-10);
-        TimerLabel.setTranslateX(590);
+        TimerLabel.setPrefSize(150,150);
         TimerLabel.setTextAlignment(TextAlignment.CENTER);
 
+        //Lobby Label
+
+        Label LobbyLabel = new Label(" Lobby");
+        LobbyLabel.setId("DefaultButtonBlack");
+        LobbyLabel.setPrefSize(200,100);
+        LobbyLabel.setTranslateY(-330);
 
 
 
-        //Un Borderpane per domarli, un BorderPane per trovarli,
-        //Un BorderPane per ghermirli e nel buio incatenarli,
-        BorderPane LobbyPane = new BorderPane();
+
+        //StackPane che contiene tutti i nodi
+        StackPane LobbyPane = new StackPane();
         LobbyPane.setId("GamemodeSelectionScreen");
+        LobbyPane.getChildren().addAll(Players,TimerLabel,LobbyLabel);
         LobbyPane.getStylesheets().addAll(this.getClass().getResource("form.css").toExternalForm());
-        LobbyPane.setTop(TimerLabel);
-        LobbyPane.setCenter(Players);
 
         LobbyScene= new Scene(LobbyPane,1280,720);
 
@@ -173,23 +180,19 @@ public class MultiplayerGUI extends Stage {
             switch(numOfPlayers){
                 case 4:
                     Player4Label.setText(LocalModel.getInstance().getClientPlayerArrayList().get(3).getName());
-                    Player4BTN.setId("ReadyBTN");
-                    Player4BTN.setText("READY!");
+                    Player4BTN.setId("ReadyPlayer4");
                     Player4BTN.setDisable(false);
                 case 3:
                     Player3Label.setText(LocalModel.getInstance().getClientPlayerArrayList().get(2).getName());
-                    Player3BTN.setId("ReadyBTN");
-                    Player3BTN.setText("READY!");
+                    Player3BTN.setId("ReadyPlayer3");
                     Player3BTN.setDisable(false);
                 case 2:
                     Player2Label.setText(LocalModel.getInstance().getClientPlayerArrayList().get(1).getName());
-                    Player2BTN.setId("ReadyBTN");
-                    Player2BTN.setText("READY!");
+                    Player2BTN.setId("ReadyPlayer2");
                     Player2BTN.setDisable(false);
                 case 1:
                     Player1Label.setText(LocalModel.getInstance().getClientPlayerArrayList().get(0).getName());
-                    Player1BTN.setId("ReadyBTN");
-                    Player1BTN.setText("READY!");
+                    Player1BTN.setId("ReadyPlayer1");
                     Player1BTN.setDisable(false);
             }
         });
