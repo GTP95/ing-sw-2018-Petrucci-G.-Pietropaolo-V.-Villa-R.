08536a4336c365268bf132f1ad3bed9e576fc206
Roberto Villa <roberto12.villa@mail.polimi.it>
2018-07-02 15:55:55 +0200

    Modificata ClientDice

--- src/main/java/Progetto_Ing_Sw/com/client/ClientDice.java
+++ src/main/java/Progetto_Ing_Sw/com/client/ClientDice.java
@@ -1,23 +1,67 @@
 package Progetto_Ing_Sw.com.client;
 
+import java.util.Objects;
+
 public class ClientDice {
     private int value;
     private int color;
-    private boolean usable;
+    //private boolean usable;
+    private boolean adjacencyBreaker;
+    private boolean colorBreaker;
+    private boolean numberBreaker;
 
     public ClientDice(int value, int color) {
         if(value<1) value=1;    //rendo totale
         if(value>6) value=6;    //la funzione
         this.value = value;
         this.color = color;
-        this.usable=true;
+        //this.usable=true;
+        this.adjacencyBreaker=false;
+        this.colorBreaker=false;
+        this.numberBreaker=false;
     }
 
     public int getValue() {
         return value;
     }
-
     public int getColor() {
         return color;
+    }
+    public boolean isAdjacencyBreaker() {return adjacencyBreaker;}
+    public boolean isColorBreaker() {return colorBreaker;}
+    public boolean isNumberBreaker() {return numberBreaker;}
+
+    public void setValue(int value) {this.value = value;}
+    public void setColor(int color) {this.color = color;}
+    public void setAdjacencyBreaker(boolean adjacencyBreaker) {this.adjacencyBreaker = adjacencyBreaker;}
+    public void setColorBreaker(boolean colorBreaker) {this.colorBreaker = colorBreaker;}
+    public void setNumberBreaker(boolean numberBreaker) {this.numberBreaker = numberBreaker;}
+
+    public int increaseValue(int value){
+
+        if(value==6){
+        }else{value++;}
+        return value;
+    }
+    public int decreaseValue(int value){
+
+        if(value==1){
+        }else{value--;}
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {       //usata per i test
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ClientDice dice = (ClientDice) o;
+        return value == dice.value &&
+                color == dice.color;
+    }
+
+    @Override
+    public int hashCode() {     //generata automaticamente da inetllij TODO: chiedere se è da togliere
+
+        return Objects.hash(value, color);
     }
 }
