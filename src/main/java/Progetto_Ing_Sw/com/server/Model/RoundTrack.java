858906eddca935bd5c322a3db02c53f1c0f7ec3a
GTP95 <giacomo.tom.petrucci@tiscali.it>
2018-07-04 19:08:26 +0200

    debug Lens Cutter

--- src/main/java/Progetto_Ing_Sw/com/server/Model/RoundTrack.java
+++ src/main/java/Progetto_Ing_Sw/com/server/Model/RoundTrack.java
@@ -42,6 +42,10 @@ public class RoundTrack {   //Implementata come singleton
 
     public Dice swapDice(Dice diceToPlace, Dice diceToGet) throws IllegalDiceException {    //Scambia uno dei dadi pescati con uno presente sulla roundTrack, usato per implementare una toolCard
         Boolean diceToPlaceExists=false;
+        for (Dice dice2 : Table.getOurInstance().getDrawnDice()) {
+            System.out.println("Dice value: "+dice2.getValue()+" color: "+dice2.getColor());
+        }
+
         for (Dice dice : Table.getOurInstance().getDrawnDice()){    //controlla che il dado che si vuole piazzare sulla roundTrack sia tra quelli pescati
             if(dice.equals(diceToPlace)){
                 diceToPlaceExists=true;
